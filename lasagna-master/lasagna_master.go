cd97624126ff6d888a917827d8d16ff3b3ac90a9
Eptarch <eptarch@gmail.com>
2021-12-17 18:59:51 +0500

    ✅ Solve lasagna-master

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -1,9 +1,36 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, avgPrepTime int) int {
+    if avgPrepTime == 0 {
+        avgPrepTime = 2
+    }
+    return len(layers) * avgPrepTime
+}
 
-// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+    noodles, sauce := 0, 0.0 
+    for _, layer := range layers {
+        switch layer {
+            case "noodles": noodles += 50
+            case "sauce": sauce += 0.2
+        }
+    }
+    return noodles, sauce
+    
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsList, myList []string) []string {
+    result := make([]string, len(myList))
+    copy(result, myList)
+    return append(result, friendsList[len(friendsList) - 1])
 
-// TODO: define the 'ScaleRecipe()' function
+}
+
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+    result := make([]float64, len(quantities))
+    copy(result, quantities)
+    for i, quantity := range quantities {
+        result[i] = quantity / 2 * float64(portions)
+    }
+    return result
+}
