06cb83e6204244ce745fadc915bc585feba3fa24
Eptarch <eptarch@gmail.com>
2021-12-18 00:13:37 +0500

    🎨 Refactor gross-store solution a bit

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -1,17 +1,15 @@
 package gross
 
-var units = map[string]int{
-    "quarter_of_a_dozen":    3,
-    "half_of_a_dozen"   :    6,
-    "dozen"             :   12,
-    "small_gross"       :  120,
-    "gross"             :  144,
-    "great_gross"       : 1728,
-}
-
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-    return units
+    return map[string]int{
+        "quarter_of_a_dozen":    3,
+        "half_of_a_dozen"   :    6,
+        "dozen"             :   12,
+        "small_gross"       :  120,
+        "gross"             :  144,
+        "great_gross"       : 1728,
+    }
 }
 
 // NewBill creates a new bill.
@@ -21,38 +19,31 @@ func NewBill() map[string]int {
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	unitValue, unitExists := units[unit]
-    if !unitExists {
+	v, ok := units[unit]
+    if !ok {
         return false
     }
-    _, itemExists := bill[item]
-    if itemExists {
-        bill[item] += unitValue
-    } else {
-        bill[item] = unitValue
-    }
+    bill[item] += v
     return true
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-    unitValue, unitExists := units[unit]
-    itemValue, itemExists := bill[item]
-    if !unitExists || !itemExists || itemValue - unitValue < 0 {
+    uv, uok := units[unit]
+    iv, iok := bill[item]
+    vdiff := iv - uv
+    if !uok || !iok || vdiff < 0 {
         return false
-    } else if itemValue - unitValue == 0 {
+    } else if vdiff == 0 {
         delete(bill, item)
     } else {
-        bill[item] -= unitValue
+        bill[item] -= uv
     }
     return true
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
-func GetItem(bill map[string]int, item string) (int, bool) {
-    itemValue, itemExists := bill[item]
-    if !itemExists {
-        itemValue = 0
-    }
-    return itemValue, itemExists
+func GetItem(slice map[string]int, item string) (int, bool) {
+    v, ok := slice[item]
+    return v, ok
 }
