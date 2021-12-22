15a00349ab8def20df0a05eae5b337ca2b6ad9d4
Eptarch <eptarch@gmail.com>
2021-12-22 23:29:25 +0500

    ✅ Solve hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,25 @@
 package hamming
 
+import "fmt"
+
+type SequenceLengthMismatch struct {
+    Diff int
+}
+
+func (e *SequenceLengthMismatch) Error() string {
+    return fmt.Sprintf("Sequences have %d items length mismatch", e.Diff)
+}
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+    diff := len(a) - len(b)
+    if diff != 0 {
+        return -1, &SequenceLengthMismatch{Diff: diff}
+    }
+    result := 0
+    for i := range a {
+        if a[i] != b[i] {
+            result++
+        }
+    }
+    return result, nil
 }
