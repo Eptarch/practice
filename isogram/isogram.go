dec995f7865bee1c4514c3f5d618a6a1453fc9e8
Eptarch <eptarch@gmail.com>
2021-12-29 00:48:18 +0500

    ✅ Solve isogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,5 +1,14 @@
 package isogram
 
+import "strings"
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+    var m = map[rune]int{}
+    for _, r := range strings.ToLower(word) {
+        m[r]++
+        if !(r == '-' || r == ' ') && m[r] > 1 {
+            return false
+        }
+    }
+    return true
 }
