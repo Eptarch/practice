394069a0f3c81920067c9efdaf936641e8eac5bd
Eptarch <eptarch@gmail.com>
2021-12-29 00:15:29 +0500

    ✅ Solve scrabble-score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,5 +1,25 @@
 package scrabble
 
+import (
+    "strings"
+)
+// LetterScore decides each letter worth.
+func LetterScore(letter rune) int {
+    switch letter {
+        case 'q', 'z': return 10
+        case 'j', 'x': return 8
+        case 'k': return 5
+        case 'f', 'h', 'v', 'w', 'y': return 4
+        case 'b', 'c', 'm', 'p': return 3
+        case 'd', 'g': return 2
+        default: return 1
+    }
+}
+// Score converts the word to lowercase and calls LetterScore on each of its letters.
 func Score(word string) int {
-  panic("Please implement the Score function")
+    r := 0
+    for _, l := range strings.ToLower(word) {
+        r += LetterScore(l)
+    }
+    return r
 }
