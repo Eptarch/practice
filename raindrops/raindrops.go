65264983f8e27ef1a347b2395e4e18794d76e0eb
Eptarch <eptarch@gmail.com>
2021-12-28 07:20:48 +0500

    ✅ Solve raindrops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,29 @@
 package raindrops
 
+import "strconv"
+
+// Pair is a replacement structure.
+type Pair struct {
+	d int
+	w string
+}
+
+var replacements = []Pair{
+	{d: 3, w: "Pling"},
+	{d: 5, w: "Plang"},
+	{d: 7, w: "Plong"},
+}
+
+// Convert number to a string considering its divisibility by 3, 5, and 7.
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	r := ""
+	for _, p := range replacements {
+		if number%p.d == 0 {
+			r += p.w
+		}
+	}
+	if r == "" {
+		return strconv.Itoa(number)
+	}
+	return r
 }
