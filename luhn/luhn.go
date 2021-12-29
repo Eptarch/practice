dbc4d3b1d5dd458143a6cd5824d8a57fe4615ed0
Eptarch <eptarch@gmail.com>
2021-12-29 06:22:12 +0500

    ✅ Solve luhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,6 +1,43 @@
 package luhn
 
-func Valid(id string) bool {
-	panic("Please implement the Valid function")
+import "unicode"
+
+func Reverse(s string) string {
+    n := len(s)
+    rs := make([]rune, n)
+    for _, r := range s {
+        n--
+        rs[n] = r
+    }
+    return string(rs[n:])
 }
 
+func Valid(id string) bool {
+    double := true
+    prep := []int{}
+    for _, r := range Reverse(id) {
+        if unicode.IsDigit(r) {
+            double = !double
+            d := int(r-'0')
+            if double {
+                d *= 2
+                if d > 9 {
+                    d -= 9
+                }
+            }
+            prep = append(prep, d)
+        } else if unicode.IsSpace(r) {
+            continue
+        } else {
+            return false
+        }
+    }
+    if len(prep) < 2 {
+        return false
+    }
+    s := 0
+    for _, v := range prep  {
+        s += v
+    }
+    return s % 10 == 0
+}
