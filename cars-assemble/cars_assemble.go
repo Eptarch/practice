a5eb6a8db1f191379be689ff2ce7cf5c8f9ee3eb
Eptarch <eptarch@gmail.com>
2022-01-15 13:57:02 +0500

    🎨 Resolve cars-assemble due to exercise update

--- cars-assemble/cars_assemble.go
+++ cars-assemble/cars_assemble.go
@@ -1,42 +1,25 @@
 package cars
 
-// SuccessRate is used to calculate the ratio of an item being created without
-// error for a given speed
-func SuccessRate(speed int) float64 {
-	if speed >= 9 {
-        return 0.77
-    } else if speed >= 5 {
-        return 0.9
-    } else if speed >= 1 {
-        return 1.0
-    } else {
-        return 0.0
-    }
-    
+const (
+    carCost uint = 10000
+    batchCost uint = 95000
+)
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+    if productionRate == 0 {return 0.0}
+    return float64(productionRate) * successRate / 100
 }
 
-// CalculateProductionRatePerHour for the assembly line, taking into account
-// its success rate
-func CalculateProductionRatePerHour(speed int) float64 {
-    if speed == 0 {return 0.0}
-    rate := SuccessRate(speed)
-    return float64(221*speed)*rate
-}
-
-// CalculateProductionRatePerMinute describes how many working items are
+// CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute
-func CalculateProductionRatePerMinute(speed int) int {
-    produced := CalculateProductionRatePerHour(speed)
-    return int(produced / 60)
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+    return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
 }
 
-// CalculateLimitedProductionRatePerHour describes how many working items are
-// produced per hour with an upper limit on how many can be produced per hour
-func CalculateLimitedProductionRatePerHour(speed int, limit float64) float64 {
-    produced := CalculateProductionRatePerHour(speed)
-    if limit < produced {
-        return limit
-    } else {
-        return produced
-    }
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	return batchCost * uint(carsCount / 10) + carCost * uint(carsCount % 10)
 }
+
