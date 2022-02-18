0eb211964b5a444ffef16fad27d43e996984a9ca
Eptarch <eptarch@gmail.com>
2022-02-18 14:50:20 +0500

    ✅ Solve error-handling

--- error-handling/error_handling.go
+++ error-handling/error_handling.go
@@ -1,5 +1,25 @@
 package erratum
 
-func Use(opener ResourceOpener, input string) error {
-	panic("Please implement the Use function")
+func Use(opener ResourceOpener, input string) (e error) {
+	resource, err := opener()
+	if err != nil {
+		switch err.(type) {
+		    case TransientError: return Use(opener, input)
+		    default: return err
+		}
+	}
+    defer resource.Close()
+	defer func() {
+		if recovery := recover(); recovery != nil {
+			switch recovery.(type) {
+			case FrobError:
+				e = recovery.(FrobError).inner
+				resource.Defrob(recovery.(FrobError).defrobTag)
+			case error:
+				e = recovery.(error)
+			}
+		}
+	}()
+	resource.Frob(input)
+	return
 }
