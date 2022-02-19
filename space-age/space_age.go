7fdbb9b6a69374d96204c234df87bc6028b6a520
Eptarch <eptarch@gmail.com>
2022-02-19 14:04:25 +0500

    ✅ Solve space-age

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -3,5 +3,16 @@ package space
 type Planet string
 
 func Age(seconds float64, planet Planet) float64 {
-	panic("Please implement the Age function")
+    var scale float64
+    switch planet {
+        case "Mercury": scale = 0.2408467
+        case "Venus": scale = 0.61519726
+        case "Earth": scale = 1.0
+        case "Mars": scale = 1.8808158
+        case "Jupiter": scale = 11.862615
+        case "Saturn": scale = 29.447498 
+        case "Uranus": scale = 84.016846 
+        case "Neptune": scale = 164.79132
+    }
+    return seconds / scale / 60 / 60 / 24 / 365.25 
 }
