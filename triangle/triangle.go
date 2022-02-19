a38bcd57d25c822be870329a86b73292742c14e2
Eptarch <eptarch@gmail.com>
2022-02-19 17:40:33 +0500

    ✅ Solve triangle

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,28 +1,18 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package triangle
 
-
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
+    NaT = iota// not a triangle
     Equ // equilateral
     Iso // isosceles
     Sca // scalene
 )
+var types = []Kind{NaT, Equ, Iso, Sca}
 
-// KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+    if a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || c + a <= b {
+        return NaT
+    }
+    return types[len(map[interface{}]float64{a: a, b: b, c: c})]
 }
