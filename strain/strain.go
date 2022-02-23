caa842289e2ec671789779970c1b54d9e9e6b5b9
Eptarch <eptarch@gmail.com>
2022-02-23 20:29:55 +0500

    ✅ Solve strain

--- strain/strain.go
+++ strain/strain.go
@@ -4,18 +4,33 @@ type Ints []int
 type Lists [][]int
 type Strings []string
 
-func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+func (i Ints) Keep(filter func(int) bool) (ns Ints) {
+    for _, n := range i {
+        if filter(n) {
+            ns = append(ns, n)
+        }
+    }
+    return
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+    return i.Keep(func(x int) bool { return !filter(x)})
 }
 
-func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+func (l Lists) Keep(filter func([]int) bool) (ls Lists) {
+    for _, lt := range l {
+        if filter(lt) {
+            ls = append(ls, lt)
+        }
+    }
+    return
 }
 
-func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+func (s Strings) Keep(filter func(string) bool) (ss Strings) {
+    for _, st := range s {
+        if filter(st) {
+            ss = append(ss, st)
+        }
+    }
+    return
 }
