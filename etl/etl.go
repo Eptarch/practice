68e6fb76534f79af0438a5f8a6a295524f1832ce
Eptarch <eptarch@gmail.com>
2022-02-24 23:12:21 +0500

    ✅ Solve etl

--- etl/etl.go
+++ etl/etl.go
@@ -1,5 +1,13 @@
 package etl
 
+import "strings"
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+    var result = map[string]int{}
+    for score, letters := range in {
+        for _, letter := range letters {
+            result[strings.ToLower(letter)] = score
+        }
+    }
+    return result
 }
