416b2e5f428018cce17475d8b97c1923abb98bd3
Eptarch <eptarch@gmail.com>
2022-02-25 19:28:03 +0500

    ✅ Solve anagram

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,25 @@
 package anagram
 
+import (
+    "strings"
+    "reflect"
+)
+
+func buildMap(s string) map[rune]int {
+    var stringmap = map[rune]int{}
+    for _, r := range strings.ToLower(s) {
+        stringmap[r] += 1
+    }
+    return stringmap
+}
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+    var result = []string{}
+    subjectmap := buildMap(subject)
+    for index, candidate := range candidates {
+        if reflect.DeepEqual(subjectmap, buildMap(candidate)) && strings.ToLower(subject) != strings.ToLower(candidate) {
+            result = append(result, candidates[index])
+        }
+    }
+    return result
 }
