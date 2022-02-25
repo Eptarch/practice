cbf78580356e4260f8db748492d03b86eb9d07fb
Eptarch <eptarch@gmail.com>
2022-02-25 18:24:27 +0500

    ✅ Solve pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,13 @@
 package pangram
 
+import "unicode"
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+    var seen = map[rune]bool{}
+    for _, r := range input {
+        if unicode.IsLetter(r) {
+            seen[unicode.ToLower(r)] = true
+        }
+    }
+    return len(seen) == 26
 }
