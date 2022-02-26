548d82b91257286e6fc6031b284bcf63a292d231
Eptarch <eptarch@gmail.com>
2022-02-26 13:15:55 +0500

    ✅ Solve word-count

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,7 +1,18 @@
 package wordcount
 
+import (
+	"regexp"
+    "strings"
+)
+
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+    r, _ := regexp.Compile(`\b[\'\w]+\b`)
+    words := r.FindAllString(phrase, -1)
+    result := Frequency{}
+    for _, word := range words {
+        result[strings.ToLower(word)]++
+    }
+    return result
 }
