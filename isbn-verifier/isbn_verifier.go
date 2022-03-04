91df06d6552c66d6dc0bae0f57ad0cadca8c24a7
Eptarch <eptarch@gmail.com>
2022-03-04 14:02:17 +0500

    ✅ Solve isbn-verifier

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,5 +1,21 @@
 package isbn
 
+import "unicode"
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	var numbers = []int{}
+    var sum int
+    for i, r := range isbn {
+        switch {
+            case unicode.IsDigit(r): numbers = append(numbers, int(r) - '0')
+            case r == 'X' && i == len(isbn) - 1: numbers = append(numbers, 10)
+        }
+    }
+    if len(numbers) != 10 {
+        return false
+    }
+    for i := 0; i < 10; i++ {
+        sum += numbers[i] * (10 - i)
+    }
+    return sum % 11 == 0
 }
