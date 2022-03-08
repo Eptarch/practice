6025cb7acea1b000bf9e753f6e4852e579a5d9d3
Eptarch <eptarch@gmail.com>
2022-03-09 01:22:30 +0500

    ✅ Solve sum-of-multiples

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,14 @@
 package summultiples
 
 func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+	var result int
+    for i := 1; i < limit; i++ { 
+        for _, divisor := range divisors {
+            if divisor != 0 && i % divisor == 0 {
+                result += i
+                break
+            }
+        }
+    }
+    return result
 }
