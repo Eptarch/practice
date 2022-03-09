340645b24a492e47ab04212782d2d60c2364475f
Eptarch <eptarch@gmail.com>
2022-03-09 12:16:22 +0500

    ✅ Solve rotational-cipher

--- rotational-cipher/rotational_cipher.go
+++ rotational-cipher/rotational_cipher.go
@@ -1,5 +1,18 @@
 package rotationalcipher
 
+import "strings"
+
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+    var sb strings.Builder
+    for _, r := range plain {
+        switch {
+            default:
+                sb.WriteRune(r)
+            case r >= 0x41 && r <= 0x5a:
+                sb.WriteRune((r - 0x41 + rune(shiftKey)) % 26 + 0x41)
+            case r >= 0x61 && r <= 0x7a:
+                sb.WriteRune((r - 0x61 + rune(shiftKey)) % 26 + 0x61)
+        }          
+    }
+    return sb.String()
 }
