b2a695e94adf97ee51b293d3568de26c10d1d059
Eptarch <eptarch@gmail.com>
2022-03-11 20:09:06 +0500

    ✅ Solve crypto-square

--- crypto-square/crypto_square.go
+++ crypto-square/crypto_square.go
@@ -1,5 +1,41 @@
 package cryptosquare
 
+import (
+	"math"
+	"strings"
+	"unicode"
+)
+
 func Encode(pt string) string {
-	panic("Please implement the Encode function")
+	var normalized = normalize(pt)
+	var rows, columns = dimensions(normalized)
+	result := make([]string, columns)
+	for i := 0; i < columns; i++ {
+		row := make([]uint8, rows)
+		for j := 0; j < rows; j++ {
+			var char uint8 = ' '
+			if len(normalized) > columns * j + i {
+				char = normalized[columns * j + i]
+			}
+			row[j] = char
+		}
+		result[i] = string(row)
+	}
+	return strings.Join(result, " ")
 }
+
+func dimensions(text string) (rows int, columns int) {
+	sqrt := math.Sqrt(float64(len(text)))
+	return int(math.Round(sqrt)), int(math.Ceil(sqrt))
+}
+
+func normalize(text string) string {
+    var normalized strings.Builder
+    for _, r := range text {
+        if unicode.IsDigit(r) || unicode.IsLetter(r) {
+            normalized.WriteRune(unicode.ToLower(r))
+        }
+    }
+	return normalized.String()
+}
+
