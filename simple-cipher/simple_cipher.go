01b8ae9c0967d3f0480ea09a232408743f04b509
Eptarch <eptarch@gmail.com>
2022-03-12 22:52:06 +0500

    ✅ Solve simple-cipher

--- simple-cipher/simple_cipher.go
+++ simple-cipher/simple_cipher.go
@@ -1,32 +1,101 @@
 package cipher
 
-// Define the shift and vigenere types here.
-// Both types should satisfy the Cipher interface.
+import (
+	"strings"
+	"unicode"
+)
+
+type shift struct {
+    distance int
+}
+
+type vigenere struct {
+    key string
+}
 
 func NewCaesar() Cipher {
-	panic("Please implement the NewCaesar function")
+    return shift{distance: 3}
 }
 
 func NewShift(distance int) Cipher {
-	panic("Please implement the NewShift function")
+    if distance < -25 || distance == 0 || distance > 25 {
+        return nil
+    }
+    return shift{distance: distance}
 }
 
 func (c shift) Encode(input string) string {
-	panic("Please implement the Encode function")
+    return shiftTranslate(normalize(input), c.distance)
 }
 
 func (c shift) Decode(input string) string {
-	panic("Please implement the Decode function")
+    return shiftTranslate(input, -c.distance)
 }
 
 func NewVigenere(key string) Cipher {
-	panic("Please implement the NewVigenere function")
+    var diff int = 0
+    for _, r := range key {
+        if r < 'a' || r > 'z' {
+            return nil
+        }
+        diff += int(r - 'a')
+    }
+    if diff <= 0 {
+        return nil
+    }
+    return vigenere{key: key}
 }
 
 func (v vigenere) Encode(input string) string {
-	panic("Please implement the Encode function")
+	return vigenereTranslate(normalize(input), v.key, true)
 }
 
 func (v vigenere) Decode(input string) string {
-	panic("Please implement the Decode function")
+	return vigenereTranslate(normalize(input), v.key, false)
+}
+
+func normalize(text string) string {
+    var normalized strings.Builder
+    for _, r := range text {
+        if unicode.IsLetter(r) {
+            normalized.WriteRune(unicode.ToLower(r))
+        }
+    }
+	return normalized.String()
+}
+
+func rotate(r rune, distance int) rune {
+	var n rune = r + rune(distance)
+	if n > 'z' {
+		n -= 'z' - 'a' + 1
+	} else if n < 'a' {
+		n += 'z' - 'a' + 1
+	}
+	return n
+}
+
+func shiftTranslate(s string, distance int) string {
+    var sb strings.Builder
+    for _, r := range s {
+        sb.WriteRune(rotate(r, distance))
+    }
+    return sb.String()
+}
+
+func vigenereTranslate(s string, key string, encode bool) string {
+    var sb strings.Builder
+	for i, r := range(s) {
+		if key[i % len(key)] == 'a' {
+			sb.WriteRune(r)
+            continue
+		} else {
+            c := NewShift(int(key[i % len(key)] - 'a'))
+            if encode {
+                sb.WriteString(c.Encode(string(r)))
+            } else {
+                sb.WriteString(c.Decode(string(r)))
+            }
+        }
+	}
+    return sb.String()
 }
