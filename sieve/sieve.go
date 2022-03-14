81adcbdd972ff9fdf7c967e2e393518792038201
Eptarch <eptarch@gmail.com>
2022-03-14 10:35:41 +0500

    ✅ Solve sieve

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,5 +1,22 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+    if limit < 2 {
+        return []int{}
+    }
+	var numbers = make([]bool, limit + 1)
+    var primes []int
+    for i := 2; i <= limit; i++ {
+        if numbers[i] == false {
+            for j := 0; i * i + i * j <= limit; j++ {
+                numbers[i * i + i * j] = true
+            }
+        }
+    }
+    for i, n := range numbers {
+        if i > 1 && n == false {
+            primes = append(primes, i)
+        }
+    }
+    return primes
 }
