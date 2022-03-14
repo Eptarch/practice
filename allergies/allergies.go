6a5150a2bc33b0bd2fa33e12de35894d065ccc63
Eptarch <eptarch@gmail.com>
2022-03-14 14:10:39 +0500

    ✅ Solve allergies

--- allergies/allergies.go
+++ allergies/allergies.go
@@ -1,9 +1,24 @@
 package allergies
 
-func Allergies(allergies uint) []string {
-	panic("Please implement the Allergies function")
+var substances = []string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}
+
+func Allergies(input uint) []string {
+	result := make([]string, 0, 8)
+	bytes := byte(input)
+	for i := 0; bytes > 0; i++ {
+		if bytes&1 == 1 {
+			result = append(result, substances[i])
+		}
+		bytes >>= 1
+	}
+	return result
 }
 
-func AllergicTo(allergies uint, allergen string) bool {
-	panic("Please implement the AllergicTo function")
+func AllergicTo(score uint, substance string) bool {
+	for index, allergen := range substances {
+		if substance == allergen {
+			return score&(1 << uint(index)) != 0
+		}
+	}
+	return false
 }
