520418657cdd50af81f2b8e39acb8ce63ddcf710
Eptarch <eptarch@gmail.com>
2022-03-14 15:38:17 +0500

    ✅ Solve perfect-numbers

--- perfect-numbers/perfect_numbers.go
+++ perfect-numbers/perfect_numbers.go
@@ -1,7 +1,49 @@
 package perfect
 
-// Define the Classification type here.
+import (
+	"errors"
+	"math"
+)
 
-func Classify(n int64) (Classification, error) {
-	panic("Please implement the Classify function")
+type Classification uint8
+
+const (
+	ClassificationPerfect Classification = iota
+	ClassificationAbundant
+	ClassificationDeficient
+)
+
+var ErrOnlyPositive = errors.New("ErrOnlyPositive")
+
+func Classify(number int64) (Classification, error) {
+	if number < 1 {
+		return ClassificationDeficient, ErrOnlyPositive
+	}
+	sum := AliquotSum(number)
+	switch {
+	    default: return ClassificationDeficient, nil
+	    case sum == number: return ClassificationPerfect, nil
+	    case sum > number: return ClassificationAbundant, nil
+	}
+}
+
+func AliquotSum(number int64) int64 {
+	sum := int64(1)
+	p := int64(math.Sqrt(float64(number)))
+	c := int64(2)
+	for number > 1 && c <= p {
+		ex := float64(1)
+		for number % c == 0 {
+			number /= c
+			ex++
+		}
+		if ex > 1 {
+			sum *= int64(math.Pow(float64(c), ex) - 1) / (c - 1)
+		}
+		c++
+	}
+	if number > 1 {
+		sum *= (number * number - 1) / (number - 1)
+	}
+	return sum / 2
 }
