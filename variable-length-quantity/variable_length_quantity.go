450989eb2e63bdfba0bad4a4a518d8a05fc1ca2a
Eptarch <eptarch@gmail.com>
2022-03-16 22:40:38 +0500

    ✅ Solve variable-length-quantity

--- variable-length-quantity/variable_length_quantity.go
+++ variable-length-quantity/variable_length_quantity.go
@@ -1,9 +1,30 @@
 package variablelengthquantity
 
-func EncodeVarint(input []uint32) []byte {
-	panic("Please implement the EncodeVarint function")
+import "fmt"
+
+func EncodeVarint(input []uint32) (result []byte) {
+    for _, n := range input {
+        encoded := []byte{byte(n&0x7f)}
+        for n >>= 7; n != 0; n >>= 7 {
+            encoded = append([]byte{byte(n&0x7f) | 0x80}, encoded...) 
+        }
+        result = append(result, encoded...)
+    }
+    return 
 }
 
-func DecodeVarint(input []byte) ([]uint32, error) {
-	panic("Please implement the EncodeVarint function")
+func DecodeVarint(input []byte) (result []uint32, err error) {
+    var number uint32
+    var complete bool
+    for _, n := range input {
+        number = (number << 7) + uint32(n&0x7f)
+        if complete = (n&0x80 == 0); complete {
+            result = append(result, number)
+            number = 0
+        }
+    }
+    if !complete {
+        return []uint32{}, fmt.Errorf("incomplete sequence")
+    }
+    return
 }
