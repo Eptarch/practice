07c5baabacb3774c09389d4bd05e4021a81ac03d
Eptarch <eptarch@gmail.com>
2022-03-16 22:55:39 +0500

    ✅ Solve armstrong-numbers

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,15 @@
 package armstrong
 
+import (
+    "math"
+    "strconv"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+    var srepr = strconv.Itoa(n)
+    var sum int
+    for _, r := range srepr {
+        sum += int(math.Pow(float64(int(r - '0')), float64(len(srepr))))
+    }
+    return n == sum
 }
