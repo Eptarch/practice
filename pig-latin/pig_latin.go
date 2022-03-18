8a7b5d190880b8c49af07ae50ec4faef12d4f41a
Eptarch <eptarch@gmail.com>
2022-03-18 23:36:28 +0500

    ✅ Solve pig-latin

--- pig-latin/pig_latin.go
+++ pig-latin/pig_latin.go
@@ -1,5 +1,32 @@
 package piglatin
 
+import (
+    "regexp"
+    "strings"
+)
+
 func Sentence(sentence string) string {
-	panic("Please implement the Sentence function")
+	var pigLatin = make([]string, 0)
+    for _, word := range strings.Fields(sentence) {
+        pigLatin = append(pigLatin, translate(word))    
+    }
+    return strings.Join(pigLatin, " ")
+}
+
+func translate(word string) string {
+    var result strings.Builder
+    if match, _ := regexp.MatchString("^(yt|xr|[aeiou]|[aeiou]qu).*", word); match {
+        result.WriteString(word)
+    } else if match, _ := regexp.MatchString("^(thr|sch|[^aeiou]qu).*", word); match {
+        result.WriteString(word[3:])
+        result.WriteString(word[:3])
+    } else if match, _ := regexp.MatchString("^(ch|qu|th|rh).*", word); match {
+        result.WriteString(word[2:])
+        result.WriteString(word[:2])
+    } else {
+        result.WriteString(word[1:])
+        result.WriteString(word[:1])
+    }
+    result.WriteString("ay")
+    return result.String() 
 }
