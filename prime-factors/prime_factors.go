98d02ebf807d4931ba1a73bf75d2eaf3904ad188
Eptarch <eptarch@gmail.com>
2022-03-18 22:37:06 +0500

    ✅ Solve prime-numbers

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,5 +1,19 @@
 package prime
 
-func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+func Factors(n int64) (factors []int64) {
+    for n % 2 == 0 {
+        factors = append(factors, 2)
+        n /= 2
+    }
+    for i := int64(3); i * i <= n; i += 2 {
+        for n % i == 0 {
+            factors = append(factors, i)
+            n /= i
+        }
+    }
+    if n > 2 {
+        factors = append(factors, n)
+    }
+    return
 }
+
