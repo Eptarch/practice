a67418cab306e34b38702b2e179729cbc9391c3e
Eptarch <eptarch@gmail.com>
2022-03-18 23:55:36 +0500

    ✅ Solve series

--- series/series.go
+++ series/series.go
@@ -1,9 +1,24 @@
 package series
 
-func All(n int, s string) []string {
-	panic("Please implement the All function")
+func All(n int, s string) (result []string) {
+    for i := 0; i <= len(s) - n; i++ {
+        result = append(result, s[i:i + n])
+    }
+    return
 }
 
 func UnsafeFirst(n int, s string) string {
-	panic("Please implement the UnsafeFirst function")
+    result, ok := First(n, s)
+    if ok {
+        return result
+    }
+    panic(nil)
 }
+
+func First(n int, s string) (first string, ok bool) {
+    if n > len(s) {
+        return s, false
+    }
+    return s[0:n], true
+}
+
