2c97e4f4b82cf22f1c02d31ee6c10c007b452401
Eptarch <eptarch@gmail.com>
2022-03-21 17:56:35 +0500

    ✅ Solve rail-fence-cipher

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -1,9 +1,37 @@
 package railfence
 
 func Encode(message string, rails int) string {
-	panic("Please implement the Encode function")
+    return Swap(message, rails, true)
 }
 
 func Decode(message string, rails int) string {
-	panic("Please implement the Decode function")
+	return Swap(message, rails, false)
+}
+
+func Swap(message string, rails int, encode bool) string {
+    var insert, index, delta int
+    var r, s *int
+    switch encode {
+        default:
+            r = &insert
+            s = &index
+        case true:
+            r = &index
+            s = &insert
+
+    }
+    cycle := (rails - 1) * 2
+    result := []byte(message)
+    for rail := 0; rail < rails; rail++ {
+        delta = rail * 2
+        for insert = rail; insert < len(message); insert += delta {
+            result[*r] = message[*s]
+            index++
+            if delta == cycle {
+                continue
+            }
+            delta = cycle - delta
+        }
+    }
+    return string(result)
 }
