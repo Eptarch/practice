dcef58d2ea9ffeb289af65af8fb53aed00c99257
Eptarch <eptarch@gmail.com>
2022-03-21 23:59:28 +0500

    ✅ Solve binary-search

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -1,5 +1,15 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+    low, high := 0, len(list) - 1
+    for low <= high {
+        middle := (low + high) / 2
+        guess := list[middle]
+        switch {
+            default: return middle
+            case guess > key: high = middle - 1
+            case guess < key: low = middle + 1
+        }
+    }
+    return -1
 }
