1e2f3fa60000f94565f25752afee889ef2cd7820
Eptarch <eptarch@gmail.com>
2022-03-22 12:30:49 +0500

    ✅ Solve binary-search-tree

--- binary-search-tree/binary_search_tree.go
+++ binary-search-tree/binary_search_tree.go
@@ -1,8 +1,3 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package binarysearchtree should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package binarysearchtree
 
 type SearchTreeData struct {
@@ -13,42 +8,47 @@ type SearchTreeData struct {
 
 // NewBst creates and returns a new SearchTreeData.
 func NewBst(i int) SearchTreeData {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return SearchTreeData{}
+    return SearchTreeData{data: i}
 }
 
 // Insert inserts an int into the SearchTreeData.
 // Inserts happen based on the rules of a BinarySearchTree
 func (std *SearchTreeData) Insert(i int) {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+    switch {
+        case i <= std.data && std.left == nil:
+            std.left = &SearchTreeData{data: i}
+        case i > std.data && std.right == nil: 
+            std.right = &SearchTreeData{data: i}
+        case i <=std.data:
+            std.left.Insert(i)
+        case i > std.data:
+            std.right.Insert(i)
+    }
 }
 
 // MapString returns the ordered contents of SearchTreeData as a []string.
 // The values are in increasing order starting with the lowest int value.
 // SearchTreeData that has the numbers [1,3,7,5] added will return the
 // []string ["1", "3", "5", "7"].
-func (std *SearchTreeData) MapString(func(int) string) (result []string) {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+func (std *SearchTreeData) MapString(f func(int) string) (result []string) {
+    ints := std.MapInt(func(i int) int { return i })
+    for _, n := range ints {
+        result = append(result, f(n))
+    }
+	return 
 }
 
 // MapInt returns the ordered contents of SearchTreeData as an []int.
 // The values are in increasing order starting with the lowest int value.
 // SearchTreeData that has the numbers [1,3,7,5] added will return the
 // []int [1,3,5,7].
-func (std *SearchTreeData) MapInt(func(int) int) (result []int) {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []int{}
+func (std *SearchTreeData) MapInt(f func(int) int) (result []int) {
+    if std.left != nil {
+        result = std.left.MapInt(f)
+    }
+    result = append(result, f(std.data))
+    if std.right != nil {
+        result = append(result, std.right.MapInt(f)...)
+    }
+	return
 }
