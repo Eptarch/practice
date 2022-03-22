4317efd2940d6d6d46a20fd41d82e5da7385733d
Eptarch <eptarch@gmail.com>
2022-03-22 17:16:15 +0500

    ✅ Solve matching-brackets

--- matching-brackets/matching_brackets.go
+++ matching-brackets/matching_brackets.go
@@ -1,5 +1,19 @@
 package brackets
 
+var t = map[rune]rune{'(': ')', '[': ']', '{': '}'}
+
 func Bracket(input string) bool {
-	panic("Please implement the Bracket function")
+    stack := []rune{}
+    for _, r := range input {
+        switch r {
+            case '(', '{', '[': stack = append(stack, r)
+            case ')', '}', ']':
+                if len(stack) == 0 || r != t[stack[len(stack) - 1]] {
+                    return false
+                }
+                stack = stack[:len(stack) - 1]
+        }
+    }
+    return len(stack) == 0
 }
+
