1be722a38a90d76c3c800b01964072c30cc8dd68
Eptarch <eptarch@gmail.com>
2022-03-29 14:37:31 +0500

    ✅ Solve diamond

--- go/diamond/diamond.go
+++ go/diamond/diamond.go
@@ -1,5 +1,23 @@
 package diamond
 
+import (
+    "bytes"
+    "errors"
+)
+
 func Gen(char byte) (string, error) {
-	panic("Please implement the Gen function")
+    if char < 'A' || char > 'Z' {
+        return "", errors.New("out of bounds")
+    }
+    distance := int(char - 'A')
+    rows := make([][]byte, 2 * distance + 1)
+    for i, j := distance, 0; i >= 0; i, j = i - 1, j + 1 {
+        line := bytes.Repeat([]byte{' '}, 2 * distance + 2)
+        line[2 * distance + 1] = '\n'
+        c := 'A' + byte(j)
+        line[i], line[2 * distance - i] = c, c
+        rows[j], rows[2 * distance - j] = line, line
+    }
+	return string(bytes.Join(rows, nil)), nil
 }
+
