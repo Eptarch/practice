61f92820d81ca1a54a5cf6d499c18650231f14af
Eptarch <eptarch@gmail.com>
2022-03-29 14:51:21 +0500

    ✅ Solve little-sisters-essay

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -1,34 +1,34 @@
-def capitalize_title(title):
+def capitalize_title(title: str) -> str:
     """
 
     :param title: str title string that needs title casing
     :return:  str title string in title case (first letters capitalized)
     """
 
-    pass
+    return title.title()
 
 
-def check_sentence_ending(sentence):
+def check_sentence_ending(sentence: str) -> str:
     """
 
     :param sentence: str a sentence to check.
     :return:  bool True if punctuated correctly with period, False otherwise.
     """
 
-    pass
+    return sentence.endswith(".")
 
 
-def clean_up_spacing(sentence):
+def clean_up_spacing(sentence: str) -> str:
     """
 
     :param sentence: str a sentence to clean of leading and trailing space characters.
     :return: str a sentence that has been cleaned of leading and trailing space characters.
     """
 
-    pass
+    return sentence.strip()
 
 
-def replace_word_choice(sentence, old_word, new_word):
+def replace_word_choice(sentence, old_word, new_word: str) -> str:
     """
 
     :param sentence: str a sentence to replace words in.
@@ -37,4 +37,5 @@ def replace_word_choice(sentence, old_word, new_word):
     :return:  str input sentence with new words in place of old words
     """
 
-    pass
+    return sentence.replace(old_word, new_word)
+
