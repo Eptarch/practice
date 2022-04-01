7180839ee731a0394129fe4667efca70b2eaa118
Eptarch <eptarch@gmail.com>
2022-04-01 14:34:26 +0500

    ✅ Solve ellens-alien-game

--- python/ellens-alien-game/classes.py
+++ python/ellens-alien-game/classes.py
@@ -1,4 +1,5 @@
 """Solution to Ellen's Alien Game exercise."""
+from typing import Any
 
 
 class Alien:
@@ -18,5 +19,26 @@ class Alien:
     teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
     collision_detection(other): Implementation TBD.
     """
+    total_aliens_created = 0
 
-    pass
+    def __init__(self, x_coordinate, y_coordinate: int, health: int = 3) -> None:
+        Alien.total_aliens_created += 1
+        self.x_coordinate, self.y_coordinate = x_coordinate, y_coordinate
+        self.health = health
+
+    def hit(self) -> None:
+        self.health -= 1
+
+    def is_alive(self) -> bool:
+        return self.health > 0
+
+    def teleport(self, x_coordinate, y_coordinate: int) -> None:
+        self.x_coordinate, self.y_coordinate = x_coordinate, y_coordinate
+
+    def collision_detection(self, other_object: Any) -> Any:
+        pass
+
+
+def new_aliens_collection(positions: list[tuple[int, int]]) -> [Alien]:
+    return [Alien(*position) for position in positions]
+
