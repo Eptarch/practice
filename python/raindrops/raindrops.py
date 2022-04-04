cc591461baf308751c5b5dc94f90b166ab6a6312
Eptarch <eptarch@gmail.com>
2022-04-04 18:33:06 +0500

    ✅ Solve raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,5 @@
-def convert(number):
-    pass
+def convert(number: int) -> str:
+    t = {3: "Pling", 5: "Plang", 7: "Plong"}
+    return "".join(
+        drop for divisor, drop in t.items() if number % divisor == 0
+    ) or str(number)
