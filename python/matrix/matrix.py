57d5e8559fa5285445c46f529dc3772a9d051006
Eptarch <eptarch@gmail.com>
2022-04-08 11:45:13 +0500

    ✅ Solve matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,12 @@
 class Matrix:
-    def __init__(self, matrix_string):
-        pass
+    def __init__(self, matrix_string: str) -> None:
+        self.content = [
+            [int(y) for y in x.split(" ")] for x in matrix_string.split("\n")
+        ]
 
-    def row(self, index):
-        pass
+    def row(self, index: int) -> [int]:
+        return self.content[index - 1]
 
-    def column(self, index):
-        pass
+    def column(self, index: int) -> [int]:
+        return [x[index - 1] for x in self.content]
+
