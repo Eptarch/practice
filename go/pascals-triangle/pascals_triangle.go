23539e6bce3d7da0fda7c7784a2de826858e74ff
Eptarch <eptarch@gmail.com>
2022-04-09 21:59:46 +0500

    ✅ Solve pascals-triangle

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,5 +1,20 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	panic("Please implement the Triangle function")
+    var result = make([][]int, n)
+    for i := 0; i < n; i++ {
+        result[i] = getRow(i)
+    }
+    return result
+}
+
+func getRow(n int) []int {
+    var result = make([]int, n + 1)
+    for i, prev, curr := 0, 1, 1; i <= n; i, prev = i + 1, curr  {
+        if i > 0 {
+            curr = (prev * (n - i + 1)) / i
+        } 
+        result[i] = curr
+    }
+    return result
 }
