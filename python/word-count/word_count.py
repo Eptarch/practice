798b65edbad5592dc80e4909263ecee0a3d77210
Eptarch <eptarch@gmail.com>
2022-04-11 23:40:38 +0500

    ✅ Solve word-count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,6 @@
-def count_words(sentence):
-    pass
+import re
+from collections import Counter
+
+
+def count_words(sentence: str) -> dict[str: int]:
+    return Counter(re.findall(r"[^\W_]+(?:'[a-z]+)?", sentence.lower()))
