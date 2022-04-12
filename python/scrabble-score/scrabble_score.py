1dc62f085131e24ab205f9eea8c7edebfcc72c15
Eptarch <eptarch@gmail.com>
2022-04-12 14:03:08 +0500

    ✅ Solve scrabble-score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,18 @@
-def score(word):
-    pass
+SCORES = {
+    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2: ["D", "G"],
+    3: ["B", "C", "M", "P"],
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"]
+}
+
+
+def score(word: str) -> int:
+    return sum(
+        [
+            score for score, letters in SCORES.items()
+            for letter in word.upper() if letter in letters
+        ]
+    )
