daa56330b0d7f3c6a4ab21a86b6ed77b4ce0a03b
Eptarch <eptarch@gmail.com>
2022-04-13 23:59:17 +0500

    ✅ Solve kindergarten-garden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,32 @@
+plants = {
+    "V": "Violets",
+    "R": "Radishes",
+    "G": "Grass",
+    "C": "Clover",
+}
+
+
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    students = (
+        "Alice",
+        "Bob",
+        "Charlie",
+        "David",
+        "Eve",
+        "Fred",
+        "Ginny",
+        "Harriet",
+        "Ileana",
+        "Joseph",
+        "Kincaid",
+        "Larry",
+    )
+
+    def __init__(self, diagram: str, students: list[str] = None) -> None:
+        if students is not None:
+            self.students = sorted(students)
+        self.diagram = diagram.splitlines()
+
+    def plants(self, student: str) -> list[str]:
+        ind = self.students.index(student) * 2
+        return [plants[pl[i]] for pl in self.diagram for i in (ind, ind + 1)]
