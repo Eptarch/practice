b998dcfca9a852c254a246adfeee89b2e9a231c4
Eptarch <eptarch@gmail.com>
2022-04-13 16:57:43 +0500

    ✅ Solve acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,7 @@
-def abbreviate(words):
-    pass
+import re
+
+
+def abbreviate(words: str) -> str:
+    return "".join(
+        [word[0] for word in re.findall(r"[A-Za-z]+(?:'[a-z]+)?", words)]
+    ).upper()
