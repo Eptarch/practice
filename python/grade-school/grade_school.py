a0a1954ae411e500b77c77b0efe1a1bb8a856bd2
Eptarch <eptarch@gmail.com>
2022-04-15 12:34:27 +0500

    ✅ Solve grade-school

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,15 +1,23 @@
 class School:
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        self.ns = dict()
+        self.log = list()
 
-    def add_student(self, name, grade):
-        pass
+    def add_student(self, name: str, grade: int) -> None:
+        if name in self.ns:
+            return self.log.append(False)
+        self.ns[name] = grade
+        self.log.append(True)
 
-    def roster(self):
-        pass
+    def roster(self) -> list[str]:
+        return [
+            a for s in [
+                self.grade(n) for n in set(self.ns.values())
+            ] for a in s
+        ]
 
-    def grade(self, grade_number):
-        pass
+    def grade(self, grade_number: int) -> list[str]:
+        return sorted(n for n, g in self.ns.items() if g == grade_number)
 
-    def added(self):
-        pass
+    def added(self) -> list[bool]:
+        return self.log
