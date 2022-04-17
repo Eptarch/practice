0894d850439af3fcf29672ac4509ed03de945d17
Eptarch <eptarch@gmail.com>
2022-04-17 11:55:27 +0500

    ✅ Solve luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,21 @@
 class Luhn:
-    def __init__(self, card_num):
-        pass
+    def __init__(self, card_num: str) -> None:
+        self.data = card_num
 
-    def valid(self):
-        pass
+    def valid(self) -> bool:
+        double = True
+        prep = []
+        for r in self.data[::-1]:
+            if r.isdigit():
+                double = not double
+                d = int(r)
+                if double:
+                    d *= 2
+                    if d > 9:
+                        d -= 9
+                prep.append(d)
+            elif not r.isspace():
+                return False
+        if len(prep) < 2:
+            return False
+        return sum(v for v in prep) % 10 == 0
