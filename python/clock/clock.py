352ac84b175658dd5f79f0a63b73faf71edfa420
Eptarch <eptarch@gmail.com>
2022-04-20 15:03:33 +0500

    ✅ Solve clock

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,18 +1,18 @@
 class Clock:
-    def __init__(self, hour, minute):
-        pass
+    def __init__(self, hour, minute: int) -> None:
+        self.m = (hour * 60 + minute) % 1440
 
-    def __repr__(self):
-        pass
+    def __repr__(self) -> str:
+        return f"Clock({self.m // 60}, {self.m % 60})"
 
-    def __str__(self):
-        pass
+    def __str__(self) -> str:
+        return f"{self.m // 60:02d}:{self.m % 60:02d}"
 
-    def __eq__(self, other):
-        pass
+    def __eq__(self, other: "Clock") -> bool:
+        return self.m == other.m
 
-    def __add__(self, minutes):
-        pass
+    def __add__(self, m: int) -> "Clock":
+        return Clock(0, self.m + m)
 
-    def __sub__(self, minutes):
-        pass
+    def __sub__(self, m: int) -> "Clock":
+        return Clock(0, self.m - m)
